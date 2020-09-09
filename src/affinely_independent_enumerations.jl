fb6c2ceb2b84ff45971bafaddd1d6514b53eb6c9
Brian Doolittle <briand4@illinois.edu>
2020-09-09 09:42:13 -0500

    error  game affinely independent enumerations

--- src/affinely_independent_enumerations.jl
+++ src/affinely_independent_enumerations.jl
@@ -1,4 +1,4 @@
-export aff_ind_success_game_strategies
+export aff_ind_success_game_strategies, aff_ind_error_game_strategies
 
 """
     aff_ind_success_game_strategies( N :: Int64, d :: Int64 ) :: Vector{Matrix{Int64}}
@@ -22,18 +22,12 @@ function aff_ind_success_game_strategies(N :: Int64, d :: Int64) :: Vector{Matri
             m[row_id,row_id] = 1
 
             for col in 1:N-1
+                col_id = col < row_id ? col : col + 1
+
                 if vec[col] == 0
-                    if col < row_id
-                        m[col,col] = 1
-                    else
-                        m[col+1,col+1] = 1
-                    end
+                    m[col_id,col_id] = 1
                 else
-                    if col < row_id
-                        m[row_id,col] = 1
-                    else
-                        m[row_id,col+1] = 1
-                    end
+                    m[row_id,col_id] = 1
                 end
             end
 
@@ -46,12 +40,124 @@ function aff_ind_success_game_strategies(N :: Int64, d :: Int64) :: Vector{Matri
     return matrices
 end
 
-function _aff_ind_vecs(num_zeros :: Int64, num_ones :: Int64) :: Vector{Vector{Int64}}
-    len = num_ones + num_zeros
+"""
+    aff_ind_error_game_strategies(N :: Int64, d :: Int64, error_size :: Int64) :: Vector{Matrix{Int64}}
 
-    aff_ind_rows = Vector{Vector{Int64}}(undef, len)
-    for i in 1:len
-        row = zeros(Int64,len)
+Enumerates an affinely independent set of vertices for the error game. The vertices
+are `N x N`, rank-`d`, column stochastic matrices.
+
+Valid inputs are `N > 4`, `N-1 > d > 1`, and `N-d+1 ≥ error_size ≥ 3`.
+"""
+function aff_ind_error_game_strategies(N :: Int64, d :: Int64, error_size :: Int64) :: Vector{Matrix{Int64}}
+    error_vecs = sort(_aff_ind_vecs(d-1, N - (error_size + d - 2))) # sorting ensures the first element has a leading 0.
+    success_vecs = _aff_ind_vecs(error_size + d - 3, N - error_size - d + 2)
+
+    matrices = Vector{Matrix{Int64}}(undef, N*(N-1))
+
+    id = 1
+    for row_id in 1:N
+
+        if row_id <= error_size
+
+            for i in 1:error_size-1
+                m = zeros(Int64, N,N)
+
+                r_id = i < row_id ? i : i + 1
+                m[r_id,row_id] = 1
+
+                m[row_id, filter(j -> j != row_id, 1:error_size)] .= 1
+
+                for col_id in (error_size+1):N
+                    el = error_vecs[1][col_id - (error_size - 1)]
+
+                    if el == 0
+                        m[col_id,col_id] = 1
+                    else
+                        m[row_id,col_id] = 1
+                    end
+                end
+
+                matrices[id] = m
+                id += 1
+            end
+
+            for vec in error_vecs[2:end]
+                m = zeros(Int64, N,N)
+
+                m[row_id, filter(j -> j != row_id, 1:error_size)] .= 1
+
+                if vec[1] == 0
+                    r_id = row_id == 1 ? 2 : 1
+                    m[r_id,row_id] = 1
+                else
+                    m[row_id,row_id] = 1
+                end
+
+                for col_id in (error_size+1):N
+                    el = vec[col_id - (error_size - 1)]
+
+                    if el == 0
+                        m[col_id,col_id] = 1
+                    else
+                        m[row_id,col_id] = 1
+                    end
+                end
+
+                matrices[id] = m
+                id += 1
+            end
+        else
+            for vec in success_vecs
+                m = zeros(Int64, N,N)
+                m[row_id,row_id] = 1
+
+                c_id = findfirst(j -> j == 1, vec)
+
+                if c_id <= error_size
+                    println
+                    m[row_id, c_id] = 1
+                    m[c_id, filter(j -> j != c_id, 1:error_size)] .= 1
+                else
+                    c_id = 1
+                    r_id = d > 2 ? 2 : 1
+
+                    m[r_id, c_id] = 1
+                    m[c_id, filter(j -> j != c_id, 1:error_size)] .= 1
+                end
+
+                for i in error_size+1:N-1
+                    el = vec[i]
+
+                    col_id = i < row_id ? i : i + 1
+                    if el == 1
+                        m[row_id,col_id] = 1
+                    else
+                        r_id = d > 2 ? col_id : c_id
+                        m[r_id,col_id] = 1
+                    end
+                    # end
+                end
+
+                matrices[id] = m
+                id += 1
+            end
+        end
+    end
+
+    return matrices
+end
+
+"""
+Helper function for `aff_ind_success_game_strategies()`. Generates a set of binary
+vectors with `num_zeros` and `num_ones`. The set contains `n` affinely independent
+vectors where `n` is the length of the binary vectors.
+"""
+function _aff_ind_vecs(num_zeros :: Int64, num_ones :: Int64) :: Vector{Vector{Int64}}
+    n = num_ones + num_zeros
+
+    aff_ind_rows = Vector{Vector{Int64}}(undef, n)
+    for i in 1:n
+        row = zeros(Int64,n)
         if i <= num_zeros + 1
             row[i] = 1
             row[num_zeros+2:end] .= 1
--- test/unit/affinely_independent_enumerations.jl
+++ test/unit/affinely_independent_enumerations.jl
@@ -15,7 +15,7 @@ using PrepareAndMeasureAnalysis
     end
 
     @testset "scanning over simple examples" begin
-        for N in 3:15
+        for N in 3:12
             for d in 2:N-1
                 strats = aff_ind_success_game_strategies(N,d)
 
@@ -32,6 +32,41 @@ using PrepareAndMeasureAnalysis
     end
 end
 
+@testset "aff_ind_error_game_strategies()" begin
+
+    @testset "simple examples" begin
+        @test aff_ind_error_game_strategies(4,2,3) == [
+            [0 1 1 1; 1 0 0 0; 0 0 0 0; 0 0 0 0],[0 1 1 1; 0 0 0 0; 1 0 0 0; 0 0 0 0],
+            [1 1 1 0; 0 0 0 0; 0 0 0 0; 0 0 0 1],[0 1 0 0; 1 0 1 1; 0 0 0 0; 0 0 0 0],
+            [0 0 0 0; 1 0 1 1; 0 1 0 0; 0 0 0 0],[0 0 0 0; 1 1 1 0; 0 0 0 0; 0 0 0 1],
+            [0 0 1 0; 0 0 0 0; 1 1 0 1; 0 0 0 0],[0 0 0 0; 0 0 1 0; 1 1 0 1; 0 0 0 0],
+            [0 0 0 0; 0 0 0 0; 1 1 1 0; 0 0 0 1],[0 1 1 0; 0 0 0 0; 0 0 0 0; 1 0 0 1],
+            [0 0 0 0; 1 0 1 0; 0 0 0 0; 0 1 0 1],[0 0 0 0; 0 0 0 0; 1 1 0 0; 0 0 1 1]
+        ]
+    end
+
+    @testset "scanning over simple cases" begin
+        for N in 4:20
+            for d in 2:N-2
+                for error_size in 3:N-d+1
+
+                    strats = aff_ind_error_game_strategies(N,d,error_size)
+                    error_game = ones(Int64, error_size, error_size) .- Matrix{Int64}(I, error_size, error_size)
+
+                    @test all(s ->
+                        rank(s) == d
+                        && error_size + d - 2 == sum(error_game[:] .* s[1:error_size,1:error_size][:]) + tr(s[error_size+1:end,error_size+1:end])
+                        && DeterministicStrategy(s) isa BellScenario.DeterministicStrategy,
+                    strats)
+
+                    @test length(strats) == N*(N-1)
+                    @test LocalPolytope.dimension(strats) == N*(N-1) - 1
+                end
+            end
+        end
+    end
+end
+
 
 @testset "_aff_ind_vecs()" begin
 
@@ -43,8 +78,8 @@ end
     end
 
     @time @testset "scanning over simple cases" begin
-        for num_zeros in 1:25
-            for num_ones in 1:25
+        for num_zeros in 1:20
+            for num_ones in 1:20
                 vecs = PrepareAndMeasureAnalysis._aff_ind_vecs(num_zeros,num_ones)
 
                 @test all( v ->
@@ -59,6 +94,5 @@ end
     end
 
 end
-
 
 end
