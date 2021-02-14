4d249bc6bd6476ee16d0236bc74a5a394031a1e0
Brian Doolittle <briand4@illinois.edu>
2021-02-14 10:59:08 -0600

    sorting edge cases for ambiguous lower bound

--- src/certifying_signaling_dimension.jl
+++ src/certifying_signaling_dimension.jl
@@ -23,67 +23,135 @@ end
 """
     ambiguous_lower_bound(P :: BellScenario.AbstractStrategy) :: Int64
 
-Uses maximum likelihood estimation where ``k`` rows are  treated as ambiguous.
-For a channel ``\\mathbf{P}\\in\\mathcal{P}^{n \\to n'}``, the ambiguous
-lower bound on the signaling dimension ``\\kappa(\\mathbf{P})`` is expressed,
+Returns the lower bound on the signaling dimension ``\\kappa(\\mathbf{P})`` as
+witnessed by facets of the ambiguous polytope ``\\mathcal{A}_{k,d}^{X \\to Y}``
+with ``k`` guessing rows and ``d`` messages of classical communication.
+That is, this method  computes the smallest ``d`` such that
+``\\mathbf{P}\\in\\mathcal{A}_{k,d}^{X\\to Y}`` for all ``k``.
+
+Formally, the  lower bound is expressed as the RHS of the following inequality
 
 ```math
-\\kappa(\\mathbf{P}) \\geq \\max_{k\\in[n']}\\max_{\\sigma\\in \\Omega_{n'}} \\sum_{y=1}^{(n'-k)} \\max_{x\\in[n]} P(\\sigma(y)|x) + \\frac{1}{n - d + 1}\\sum_{y=n'-k}^{n'}\\sum_{x\\in [n]} P(\\sigma(y)|x)
+\\kappa(\\mathbf{P}) \\geq \\max_{k\\in[Y]}\\max_{\\sigma\\in \\Omega_{Y}} \\sum_{y=1}^{(Y-k)} \\max_{x\\in[X]} P(\\sigma(y)|x) + \\frac{1}{X - d + 1}\\sum_{y=Y-k+1}^{Y}\\sum_{x\\in [n]} P(\\sigma(y)|x)
 ```
 
-where ``\\Omega_{n'}`` is the set of all permutations of ``[n']``.
-Note that considering all permutations of ``k`` ambiguous can be costly.
+where ``\\Omega_{Y}`` is the set of all permutations of ``[Y]``.
+Note that considering all permutations of ``k`` guessing rows can be costly, however,
+the performance is improved by sorting the rows of ``\\mathbf{P}`` by increasing
+row sum and max-min difference.
+A priori, it is not clear whether the row sum or max-min difference is the primary
+ordering, therefore, this method checks both.
 """
 function ambiguous_lower_bound(P :: BellScenario.AbstractStrategy) :: Int64
     (num_rows, num_cols) = size(P)
 
-    lower_bounds = map(k -> ambiguous_lower_bound(P, k), 1:num_rows)
+    sorted_P_by_row_sum = _ambiguous_lower_bound_pre_sort_primary_row_sum(P)
+    sorted_P_by_max_min_diff = _ambiguous_lower_bound_pre_sort_primary_max_min_diff(P)
+
+    ordering_max(k) = max(
+        _ambiguous_lower_bound(sorted_P_by_row_sum, k),
+        _ambiguous_lower_bound(sorted_P_by_max_min_diff, k)
+    )
+
+    lower_bounds = map(ordering_max, 1:num_rows)
 
     # the largest lower bound is the lower bound of signaling dimension
     max(lower_bounds...)
 end
 
 """
-Efficiency can be improved somewhat by just running the method for a fixed number
-of ambiguous rows.
-
     ambiguous_lower_bound(
         P :: BellScenario.AbstractStrategy,
-        num_ambiguous_rows :: Int64
+        k :: Int64
     ) :: Int64
 
-A `DomainError` is thrown if `num_ambiguous_row < 1` or `num_ambiguous_rows > size(P)[1]`.
+Finds the ambiguous lower bound on ``\\kappa(\\mathbf{P})`` for fixed ``k``.
+That is, returns the smallest ``d`` such that ``\\mathbf{P}\\in\\mathcal{A}_{k,d}^{X\\to Y}``,
+where ``\\mathcal{A}_{k,d}^{X\\to Y}`` is the ambiguous polytope with for ``k`` guessing rows.
+
+* `P` - Channel ``\\mathbf{P}\\in\\mathcal{P}^{X\\to Y}``, a column stochastic matrix.
+* `k` - The number of guessing rows.
+
+A `DomainError` is thrown if `k < 1` or `k > size(P)[1]` (number of rows in `P`).
 """
-function ambiguous_lower_bound(P :: BellScenario.AbstractStrategy, num_ambiguous_rows :: Int64) :: Int64
+function ambiguous_lower_bound(P :: BellScenario.AbstractStrategy, k :: Int64) :: Int64
     (num_rows, num_cols) = size(P)
 
-    if !(1 ≤ num_ambiguous_rows ≤ num_rows)
-        throw(DomainError(num_ambiguous_rows, "input `num_ambiguous_rows` must be in range [1, $num_rows]."))
+    if !(1 ≤ k ≤ num_rows)
+        throw(DomainError(k, "input `k` must be in range [1, $num_rows]."))
     end
 
-    num_guessing_rows = num_rows - num_ambiguous_rows
+    sorted_P_by_row_sum = _ambiguous_lower_bound_pre_sort_primary_row_sum(P)
+    sorted_P_by_max_min_diff = _ambiguous_lower_bound_pre_sort_primary_max_min_diff(P)
 
+    max(
+        _ambiguous_lower_bound(sorted_P_by_row_sum, k),
+        _ambiguous_lower_bound(sorted_P_by_max_min_diff,k)
+    )
+end
+
+"""
+    _ambiguous_lower_bound_pre_sort_primary_row_sum(P :: AbstractMatrix) :: AbstractMatrix
+
+Sort matrix `P` by
+1. (Primary): The sum of the row.
+2. (Secondary): The difference between the row maximum and minimum.
+"""
+function _ambiguous_lower_bound_pre_sort_primary_row_sum(P :: AbstractMatrix)
+    # pre-sort rows by (max - min)
+    max_min_diff(row) = max(row...)-min(row...)
+    rows_sorted_by_diff = sortslices(P, dims=1, by=max_min_diff, rev=true)
+
+    # sort channel by the row sum so that the largest row sum is last.
+    rows_sorted_by_sum = sortslices(rows_sorted_by_diff, dims=1, by=sum)
+
+    rows_sorted_by_sum
+end
+
+"""
+    _ambiguous_lower_bound_pre_sort_primary_max_min_diff(P :: AbstractMatrix) :: AbstractMatrix
+
+Sort matrix `P` by
+1. (Primary): The difference between the row maximum and minimum.
+2. (Secondary): The sum of the row.
+"""
+function _ambiguous_lower_bound_pre_sort_primary_max_min_diff(P :: AbstractMatrix)
     # pre-sort channel by the row sum so that the largest row sum is last.
     rows_sorted_by_sum = sortslices(P, dims=1, by=sum)
 
-    # sort rows by max-min
+    # sort rows by (max - min)
     max_min_diff(row) = max(row...)-min(row...)
-    rows_sorted_by_dif = sortslices(rows_sorted_by_sum, dims=1, by=max_min_diff, rev=true)
+    rows_sorted_by_diff = sortslices(rows_sorted_by_sum, dims=1, by=max_min_diff, rev=true)
+
+    rows_sorted_by_diff
+end
+
+"""
+    _ambiguous_lower_bound(P :: AbstractMatrix, k :: Int64) :: Int64
+
+Returns the smallest integer `d` such that `P` is contained by the ambiguous guessing
+game with the first `k` rows as guessing rows and the remaining as ambiguous.
+
+* `P` - column stochastic matrix.
+* `k` - number of guessing rows to consider.
+"""
+function _ambiguous_lower_bound(P :: AbstractMatrix, k :: Int64) :: Int64
+    (num_rows,num_cols) = size(P)
 
     # take k guessing guessing rows with largest max-min
-    ml_sum = (num_guessing_rows > 0) ? sum(y -> max(rows_sorted_by_dif[y,:]...), 1:num_guessing_rows) : 0
-    ambiguous_sum = sum(y -> sum(rows_sorted_by_dif[y,:]), num_guessing_rows+1:num_rows)
+    ml_sum = (k > 0) ? sum(y -> max(P[y,:]...), 1:k) : 0
+    ambiguous_sum = (k < num_rows) ? sum(y -> sum(P[y,:]), k+1:num_rows) : 0
 
     # d > ml_sum
     d_min = ceil(ml_sum)
 
+    # Increment d until no violation occurs.
+    # if violation occurs then signaling dimension is greater than d
     lower_bound = 1
     for d in d_min:min(size(P)...)
-        # violation occurs, signaling dimension is greater than d
         score = ml_sum + ambiguous_sum/(num_cols - d + 1)
 
         if (score < d) || (score ≈ d)
-            println(score)
             lower_bound = d
             break
         end
--- test/unit/certifying_signaling_dimension.jl
+++ test/unit/certifying_signaling_dimension.jl
@@ -20,8 +20,8 @@ end
             0.3  0.3  0.3;
         ])
 
-        @test 2 == ambiguous_lower_bound(P, 1)
-        @test 3 == ambiguous_lower_bound(P, 2)
+        @test 2 == ambiguous_lower_bound(P, 4)
+        @test 3 == ambiguous_lower_bound(P, 3)
 
         # test over all numbers of ambiguous rows
         @test 3 == ambiguous_lower_bound(P)
@@ -31,6 +31,85 @@ end
         P = DeterministicStrategy([1 0 0 1;0 1 1 0;0 0 0 0;0 0 0 0])
 
         @test 2 == ambiguous_lower_bound(P)
+    end
+
+    @testset "pre-sorting edge cases" begin
+        @testset "edge case: primary sort by row sum, secondary by max-min diff is not optimal" begin
+            P1 = Strategy([
+                0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1;
+                0.9 0   0   0   0   0   0   0  ;
+                0   0.9 0   0   0   0   0   0  ;
+                0   0   0.9 0   0   0   0   0  ;
+                0   0   0   0.9 0   0   0   0  ;
+                0   0   0   0   0.9 0   0   0  ;
+                0   0   0   0   0   0.9 0   0  ;
+                0   0   0   0   0   0   0.9 0  ;
+                0   0   0   0   0   0   0   0.9;
+            ])
+
+            P2 = Strategy([
+                0.9 0   0   0   0   0   0   0  ;
+                0   0.9 0   0   0   0   0   0  ;
+                0   0   0.9 0   0   0   0   0  ;
+                0   0   0   0.9 0   0   0   0  ;
+                0   0   0   0   0.9 0   0   0  ;
+                0   0   0   0   0   0.9 0   0  ;
+                0   0   0   0   0   0   0.9 0  ;
+                0   0   0   0   0   0   0   0.9;
+                0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1;
+            ])
+
+
+            @test 7 == SignalingDimension._ambiguous_lower_bound(P1,8)
+            @test 8 == SignalingDimension._ambiguous_lower_bound(P2,8)
+
+            @test 8 == ambiguous_lower_bound(P1,8)
+            @test 8 == ambiguous_lower_bound(P2,8)
+        end
+
+        @testset "edge case: primary sort by row sum, secondary by max-min diff is optimal" begin
+            # primary sort by max - min diff, secondary by row sum
+            P1a  = Strategy([
+                0 0.1 0.5 0;
+                0.6 0.6 0.5 1;
+                0 0.3 0 0;
+                0.4 0 0 0;
+            ])
+            P1b = Strategy([
+                0.4 0.4 1 1 1 1 1 1 1 1;
+                0.3 0 0 0 0 0 0 0 0 0;
+                0.3 0 0 0 0 0 0 0 0 0;
+                0 0.3 0 0 0 0 0 0 0 0;
+                0 0.3 0 0 0 0 0 0 0 0;
+            ])
+
+            #  primary sort by row sum, secondary by max-min diff
+            P2a = Strategy([
+                0 0.3 0 0;
+                0.4 0 0 0;
+                0 0.1 0.5 0;
+                0.6 0.6 0.5 1;
+            ])
+            P2b = Strategy([
+                0.3 0 0 0 0 0 0 0 0 0;
+                0.3 0 0 0 0 0 0 0 0 0;
+                0 0.3 0 0 0 0 0 0 0 0;
+                0 0.3 0 0 0 0 0 0 0 0;
+                0.4 0.4 1 1 1 1 1 1 1 1;
+            ])
+
+            @test 2 == SignalingDimension._ambiguous_lower_bound(P1a,3)
+            @test 3 == SignalingDimension._ambiguous_lower_bound(P2a,3)
+
+            @test 2 == SignalingDimension._ambiguous_lower_bound(P1b,4)
+            @test 3 == SignalingDimension._ambiguous_lower_bound(P2b,4)
+
+            @test 3 == ambiguous_lower_bound(P1a,3)
+            @test 3 == ambiguous_lower_bound(P2a,3)
+
+            @test 3 == ambiguous_lower_bound(P1b,4)
+            @test 3 == ambiguous_lower_bound(P2b,4)
+        end
     end
 
     @testset "errors" begin
@@ -53,7 +132,7 @@ end
     @testset "strategy where ambiguous game must be tested" begin
         P = Strategy(1/2*[1 0 0;0 1 0;0 0 1;1 1 1])
         @test 2 == maximum_likelihood_lower_bound(P)
-        @test 3 == ambiguous_lower_bound(P,1)
+        @test 3 == ambiguous_lower_bound(P,3)
         @test attains_trivial_upper_bound(P)
     end
 end
